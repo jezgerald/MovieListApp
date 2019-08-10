02d5fb50fcac9982adedcc674d96213677b12e9a
Jess <jess_uni10@hotmail.com>
2019-08-10 19:35:29 +1000

    * MainPage.xaml: - Updated cell layout and changed button text
    * MainPage.xaml.cs: - Changed 'More' to 'Watched'
    - Added OnDelete method
    - Attempted to sort list - not working

--- MovieListApp/MainPage.xaml.cs
+++ MovieListApp/MainPage.xaml.cs
@@ -32,37 +32,43 @@ namespace MovieListApp
             {
                 await DisplayAlert("Movie added", "Movie - " + MovieEntry.Text, "OK");
                 movieItems.Add(MovieEntry.Text);
+                MovieEntry.Text = "";
             }
         }
+
+        public void OnMore(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            DisplayAlert("Watched", "Mark " + mi.CommandParameter + " as watched?", "OK");
+
+            var strikethroughLabel = new Label { Text = "This is text with strikethrough.", TextDecorations = TextDecorations.Strikethrough };
+            MyListView.SelectedItem = strikethroughLabel;
+
+            //var layout = new StackLayout { Padding = new Thickness(5, 10) };
+            //var label = new Label { Text = "This is a green label.", TextColor = Color.FromHex("#77d065"), FontSize = 20 };
+            //layout.Children.Add(label);
+            //this.Content = layout;
+        }
+
+        public void OnDelete(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            DisplayAlert("Delete?", "Delete " + mi.CommandParameter + " from list?", "OK");
+            
+            movieItems.Remove((string)MyListView.SelectedItem);
+        }
+
+        // NOT WORKING
+        //private async void Sort(object sender, EventArgs e)
+        //{
+        //    if (movieItems.Count == 0)
+        //    {
+        //        await DisplayAlert("Cannot sort", "No items available to sort", "OK");
+        //    }
+        //    else
+        //    {
+        //        movieItems = new ObservableCollection<string>(movieItems.OrderBy(a => a));
+        //    }
+        //}
     }
 }
-
-
-
-//    <StackLayout>
-//        <Entry Placeholder = "Enter a movie title" x:Name="MovieEntry"/>
-//        <Button Text = "Done" Clicked="OnClick"/>
-//        <ListView x:Name= "MyListView"/>
-//    </StackLayout>
-
-
-
-//        public ObservableCollection<string> movieItems = new ObservableCollection<string>();
-
-//public MainPage()
-//{
-//    InitializeComponent();
-//    MyListView.ItemsSource = movieItems;
-//}
-
-//private async void OnClick(object sender, EventArgs e)
-//{
-//    if (string.IsNullOrWhiteSpace(MovieEntry.Text))
-//    {
-//        await DisplayAlert("Empty field", "Please add a title to the entry field", "OK");
-//    }
-//    else
-//    {
-//        movieItems.Add(MovieEntry.Text);
-//    }
-//}
