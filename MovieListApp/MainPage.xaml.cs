c68a54aef18797c95ee52b4672a4867d0ec50b84
Jess <jess_uni10@hotmail.com>
2019-08-11 19:11:20 +1000

    Added app icon images to iOS version
    Changed xaml:
    - Added Title
    - Added Search bar
    - Commented Sort and Watched buttons
    
    Changed xaml.cs
    - Added search function
    - Commented Sort and Watched functions
    - Modified Delete function - worked with Android but not iOS

--- MovieListApp/MainPage.xaml.cs
+++ MovieListApp/MainPage.xaml.cs
@@ -18,6 +18,11 @@ namespace MovieListApp
 
         public MainPage()
         {
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                Padding = new Thickness(0, 40, 0, 0);
+            }
+
             InitializeComponent();
             MyListView.ItemsSource = movieItems;
         }
@@ -36,31 +41,38 @@ namespace MovieListApp
             }
         }
 
-        public void OnMore(object sender, EventArgs e)
-        {
-            var mi = ((MenuItem)sender);
-            DisplayAlert("Watched", "Mark " + mi.CommandParameter + " as watched?", "OK");
-
-            var strikethroughLabel = new Label { Text = "This is text with strikethrough.", TextDecorations = TextDecorations.Strikethrough };
-            MyListView.SelectedItem = strikethroughLabel;
-
-            //var layout = new StackLayout { Padding = new Thickness(5, 10) };
-            //var label = new Label { Text = "This is a green label.", TextColor = Color.FromHex("#77d065"), FontSize = 20 };
-            //layout.Children.Add(label);
-            //this.Content = layout;
-        }
 
         public void OnDelete(object sender, EventArgs e)
         {
             var mi = ((MenuItem)sender);
             DisplayAlert("Delete?", "Delete " + mi.CommandParameter + " from list?", "OK");
-            
-            movieItems.Remove((string)MyListView.SelectedItem);
+
+            var item = (string)mi.CommandParameter;
+            movieItems.Remove(item);
         }
 
-        // NOT WORKING
+        private void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            MyListView.BeginRefresh();
+
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+            {
+                DisplayAlert("Error", "List needs items to search!", "OK");
+                MyListView.ItemsSource = movieItems;
+            }
+            else
+            {
+                MyListView.ItemsSource = movieItems.Where(i => i.Contains(e.NewTextValue));
+            }
+
+            MyListView.EndRefresh();
+        }
+
+        // Sort function - NOT WORKING
         //private async void Sort(object sender, EventArgs e)
         //{
+        //    MyListView.BeginRefresh();
+
         //    if (movieItems.Count == 0)
         //    {
         //        await DisplayAlert("Cannot sort", "No items available to sort", "OK");
@@ -69,6 +81,23 @@ namespace MovieListApp
         //    {
         //        movieItems = new ObservableCollection<string>(movieItems.OrderBy(a => a));
         //    }
+
+        //    MyListView.EndRefresh();
+        //}
+
+                // no longer using
+        //public void OnMore(object sender, EventArgs e)
+        //{
+        //    var mi = ((MenuItem)sender);
+        //    DisplayAlert("Watched", "Mark " + mi.CommandParameter + " as watched?", "OK");
+
+        //    var strikethroughLabel = new Label { Text = "This is text with strikethrough.", TextDecorations = TextDecorations.Strikethrough };
+        //    MyListView.SelectedItem = strikethroughLabel;
+
+        //    //var layout = new StackLayout { Padding = new Thickness(5, 10) };
+        //    //var label = new Label { Text = "This is a green label.", TextColor = Color.FromHex("#77d065"), FontSize = 20 };
+        //    //layout.Children.Add(label);
+        //    //this.Content = layout;
         //}
     }
 }
